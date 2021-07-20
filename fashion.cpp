f46909dfc8b4c153cd7cd2fd3081cc8c16ff0b24
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-07-20 22:31:18 +0530

    12th ques
    Fashion Shows

---
+++ fashion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int *m = new int[n];
+        int *w = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> m[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> w[i];
+        }
+        sort(m, m + n);
+        sort(w, w + n);
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += m[i] * w[i];
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
